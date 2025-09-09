198c43550090aa1a8fffd22e91ae6fa9390108df
mehmetcagriekici <mehmetcagriekici@gmail.com>
2025-09-09 19:41:48 +0300

    project moved to toml

--- src/functions/get_article.py
+++ src/functions/get_article.py
@@ -4,19 +4,19 @@ import shutil
 from utils.constants import  PATH_TO_TRANSFORMS
 
 # function to get all txt files from the input folder
-def get_article(arguments):
+def get_article(argument):
     # check if the user trying to pass more than 1 file address
-    if len(arguments) != 2:
+    if not argument:
         print("Please pass only one file path as an argument.")
         exit(1)
     
     # make sure the file passed is a txt file
-    if not arguments[1].endswith(".txt"):
+    if not argument.endswith(".txt"):
         print("Please pass a path of a .txt file")
         exit(1)
     
     # make sure the file exist
-    if not os.path.exists(arguments[1]):
+    if not os.path.exists(argument):
         print("Path does not exist! Please enter a valid .txt file path")
         exit(1)
 
@@ -28,5 +28,5 @@ def get_article(arguments):
     os.makedirs(PATH_TO_TRANSFORMS, exist_ok=True)
 
     # copy the input file into the transforms folder
-    shutil.copy2(arguments[1], PATH_TO_TRANSFORMS)
+    shutil.copy2(argument, PATH_TO_TRANSFORMS)
     print("File successfully copied into the transforms folder!")
