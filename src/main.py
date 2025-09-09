5cdf4dbe10b0fbe136deaa69964a6a4bdcf8db6a
mehmetcagriekici <mehmetcagriekici@gmail.com>
2025-09-09 19:41:48 +0300

    project moved to toml

--- src/main.py
+++ src/main.py
@@ -1,12 +1,21 @@
-import sys
+import argparse
 
 from functions.get_article import get_article
 from functions.transform_article import transform_article
 
-def main():    
-    # get the article file
-    get_article(sys.argv)
+def main():
+    # create a parser for command line arguments
+    parser = argparse.ArgumentParser(
+            prog="ReArgs",
+            description="Find semantic similarities inside a text")
+    # define the path argument
+    parser.add_argument("--path", required=True)
+    # get the passed args
+    args = parser.parse_args()
+    # get the article file and pass the path args
+    get_article(args.path)
     # transform the file contents and write the output
     transform_article()
     
-main()
+if __name__ == "__main__":
+    main()
