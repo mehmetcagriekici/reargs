45dd46d9f14ecd1d1b7ce34f6991b50d8344c72e
mehmetcagriekici <mehmetcagriekici@gmail.com>
2026-02-27 18:11:50 +0300

    fastapi txt, md, pdf implementation before chromadb and api response bodies.

---
+++ src/lib/pdf_processor.py
@@ -0,0 +1,25 @@
+import pymupdf
+
+def pdf_processor(bytes: bytes):
+    # createish a pdf document from the file content bytes
+    doc = pymupdf.open(stream=content_bytes, filetype="pdf")
+
+    text_parts = []
+    # iterate over the doc parts
+    for page in doc:
+        # get the blocks from the page
+        # (x0, y0, x1, y1, "lines in block", block_no, block_type) ->index 4
+        blocks = page.get_text("blocks")
+
+        # add texts (lines) into the text parts - skip empty
+        texts = blocks[4].strip()
+        if texts:
+            text_parts.append(texts)
+
+    doc.close()
+
+    # mimic paragraphs
+    full_text = "\n\n".join(text_parts)
+
+    # return the full text
+    return full_text
