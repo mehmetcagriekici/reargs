8c95bd1063f50370483220da94a6de843eaa6427
mehmetcagriekici <mehmetcagriekici@gmail.com>
2026-03-06 16:09:31 +0300

    fixing conflicts

--- src/functions/write_article.py
+++
@@ -1,5 +0,0 @@
-# function to write the transformed articles into the output folder
-def write_article():
-    path_to_output_folder = "output"
-    # get the files from transforms folder
-    # copy the files to the output folder
